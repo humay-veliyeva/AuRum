ab19672537d81e4b59c2a1d963ed9d0656e00c86
Veliyeva Humay <103187385+humay-veliyeva@users.noreply.github.com>
2023-03-10 15:55:30 +0400

    hazirdir

---
+++ assets/js/app.js
@@ -0,0 +1,98 @@
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.visibility = "visible";
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.visibility = "hidden";
+    mybutton.style.opacity = "0";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+const mobil = document.querySelector('.mobil');
+const closeBtn = document.querySelector('.close-btn');
+const openBar = document.querySelector('.bx-menu')
+
+openBar.addEventListener('click', () => {
+    mobil.style = `
+    visibility: visible;
+    height: 100%;
+    max-height: 100%;
+    opacity: 1;
+    transition: linear 300ms;
+    `
+})
+
+closeBtn.addEventListener('click', () => {
+    mobil.style = `
+    visibility: hidden;
+    height:0;
+    max-height: none;
+    transition: linear 200ms;
+    `
+})
+
+const carrer = document.querySelectorAll('.carrer-top');
+const carrerItem = document.querySelectorAll('.carrer-item');
+const carrerBottomContent = document.querySelectorAll('.carrer-text');
+const arrow = document.querySelectorAll('.bx-chevron-right')
+
+carrer.forEach((el, item) => {
+    el.addEventListener('click', () => {
+        carrerBottomContent[item].classList.toggle('active')
+        arrow[item].classList.toggle('active')
+    })
+})
+
+arrow.forEach((el, item) => {
+    el.addEventListener('click', () => {
+        carrerBottomContent[item].classList.toggle('active')
+        arrow[item].classList.toggle('active')
+    })
+})
+
+const current_year = document.querySelector('.current-year');
+const year = document.querySelector('.year');
+const yearInner = document.querySelectorAll('.year-txt');
+
+current_year.innerHTML = '2023 <i class="fa-solid fa-angle-down"></i>'
+
+yearInner.forEach((el, item) => {
+    el.addEventListener('click', () => {
+        let currentYear = el.innerHTML
+        current_year.innerHTML = currentYear + '<i class="fa-solid fa-angle-down"></i>'
+        year.classList.remove('active')
+    })
+})
+
+
+current_year.addEventListener('click', () => {
+    year.classList.toggle('active')
+})
+
+const cv = document.querySelector('.cv-area');
+
+cv.addEventListener('click', () => {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = _ => {
+        let files = Array.from(input.files);
+        console.log(files);
+    };
+    input.click();
+})
